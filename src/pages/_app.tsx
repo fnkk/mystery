77787d88bf8fc6efe0708d52c4833f07162f8238
cppppppp7 <cp@artela.network>
2024-05-11 20:31:30 +0800

    ..

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const { type } = router.query;
 
-  let ogImage = 'https://trueart-content.oss-cn-shanghai.aliyuncs.com/20190518/174134957_640.jpg';
+  let ogImage = 'https://s.rfi.fr/media/display/8a79660e-8d2f-11ec-8878-005056a90321/w:1280/p:16x9/maxresdefault-54.jpg';
   if (type === 'a') {
     ogImage = 'https://s.rfi.fr/media/display/8a79660e-8d2f-11ec-8878-005056a90321/w:1280/p:16x9/maxresdefault-54.jpg';
   } else if (type === 'b') {
@@ -18,7 +18,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta property="og:title" content="mystery" />
         <meta property="og:description" content="It's right if you don't understand, as there are usually some mysterious things hidden here." />
-        <meta property="og:type" content="website" />
+        <meta property="og:type" content="article" />
         <meta property="og:image" content={ogImage} />
       </Head>
       <Component {...pageProps} />
